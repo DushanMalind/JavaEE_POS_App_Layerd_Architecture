a8e3a007020de7bf22f84f90af2aa0de5c422e3e
DushanMalind <dushanmalinda010@gmail.com>
2023-09-05 13:01:37 +0530

    Back End

---
+++ JavaEE_POS_App_layerd/Back_End/src/lk/ijse/javaPos/layerd/Main.java
@@ -0,0 +1,10 @@
+package lk.ijse.javaPos.layerd;
+
+/**
+ * @authority DUSHAN MALINDA
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
