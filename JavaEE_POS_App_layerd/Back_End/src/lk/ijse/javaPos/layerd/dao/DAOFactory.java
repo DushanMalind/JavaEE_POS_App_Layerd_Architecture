e147c5c0110dec3bdabea7138c541c899a21ede6
DushanMalind <dushanmalinda010@gmail.com>
2023-09-06 11:48:43 +0530

    DaoFactory create

--- JavaEE_POS_App_layerd/Back_End/src/lk/ijse/javaPos/layerd/dao/DAOFactory.java
+++ JavaEE_POS_App_layerd/Back_End/src/lk/ijse/javaPos/layerd/dao/DAOFactory.java
@@ -1,8 +1,43 @@
 package lk.ijse.javaPos.layerd.dao;
 
+import lk.ijse.javaPos.layerd.dao.custom.impl.CustomerDAOImpl;
+import lk.ijse.javaPos.layerd.dao.custom.impl.ItemDAOImpl;
+import lk.ijse.javaPos.layerd.dao.custom.impl.OrderDetailsDAOImpl;
+import lk.ijse.javaPos.layerd.dao.custom.impl.OrdersDAOImpl;
+
 /**
  * @authority DUSHAN MALINDA
  */
 public class DAOFactory {
 
+    private static DAOFactory daoFactory;
+
+    private DAOFactory() {
+    }
+
+    public static DAOFactory getDaoFactory(){
+        if (daoFactory==null){
+            daoFactory=new DAOFactory();
+        }
+        return daoFactory;
+    }
+
+    public enum DAOTypes{
+        CUSTOMER,ITEM,ORDERS,ORDERDETAILS;
+    }
+
+    public SuperDAO geDao(DAOTypes daoTypes){
+        switch (daoTypes){
+            case CUSTOMER:
+                return new CustomerDAOImpl();
+            case ITEM:
+                return new ItemDAOImpl();
+            case ORDERS:
+                return new OrdersDAOImpl();
+            case ORDERDETAILS:
+                return new OrderDetailsDAOImpl();
+
+        }
+        return null;
+    }
 }
---
+++ JavaEE_POS_App_layerd/Back_End/web/index.jsp
@@ -0,0 +1,18 @@
+<%--
+&lt;%&ndash;
+  Created by IntelliJ IDEA.
+  User: ASUS
+  Date: 05/09/2023
+  Time: 14:00
+  To change this template use File | Settings | File Templates.
+&ndash;%&gt;
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
+--%>
