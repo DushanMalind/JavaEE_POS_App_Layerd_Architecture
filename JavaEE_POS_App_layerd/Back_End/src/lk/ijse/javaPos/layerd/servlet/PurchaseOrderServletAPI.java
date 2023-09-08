3c2729f01c9c099c16ea1f0440de9e1c30236f17
DushanMalind <dushanmalinda010@gmail.com>
2023-09-08 13:53:32 +0530

    PurchaseOrder add cart and data load done

---
+++ JavaEE_POS_App_layerd/Back_End/src/lk/ijse/javaPos/layerd/servlet/PurchaseOrderServletAPI.java
@@ -0,0 +1,12 @@
+package lk.ijse.javaPos.layerd.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @authority DUSHAN MALINDA
+ */
+
+@WebServlet(urlPatterns = "/purchaseOrder")
+public class PurchaseOrderServletAPI extends HttpServlet {
+}
